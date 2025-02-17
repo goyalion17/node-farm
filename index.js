27b61f666257f17294ee0def965d5b7d1ded01a9
Iryna Savelieva <irynasavelieva@Mac.local>
2025-02-17 12:09:29 +0100

    Using Modules 2: Our Own Modules

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require("fs")
 const http = require('http')
 const url = require('url')
+const replaceTemplate = require('./modules/replaceTemplate')
 
 //////////////////////////////
 //////// FILES
@@ -31,19 +32,6 @@ console.log('File written!') */
 
 //////////////////////////////
 //////// SERVER
-const replaceTemplate = (temp, product) => {
-    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName)
-    output = output.replace(/{%IMAGE%}/g, product.image)
-    output = output.replace(/{%PRICE%}/g, product.price)
-    output = output.replace(/{%FROM%}/g, product.from)
-    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients)
-    output = output.replace(/{%QUANTITY%}/g, product.quantity)
-    output = output.replace(/{%DESCRIPTION%}/g, product.description)
-    output = output.replace(/{%ID%}/g, product.id)
-
-    if (!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic')
-    return output
-}
 
 const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8')
 const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8')
@@ -68,7 +56,7 @@ const server = http.createServer((req, res) => {
         const product = dataObj[query.id]
         const output = replaceTemplate(tempProduct, product)
         res.end(output)
-        
+
     // API
     } else if (pathname === '/api') {
         res.writeHead(200, {'Content-type': 'application/json'})
